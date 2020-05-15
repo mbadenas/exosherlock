4179ec24c8b300be3bcc1e0ba4c996fe7f08245c
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2020-05-15 19:24:49 +0200

    add reading database scripts

---
+++ sherlock/database.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import pandas as pd
+
+def get_local_exoarchive(fname=None):
+    if fname is None:
+        dirname = Path(__file__).parent / "data"
+        files = [item for item in dirname.iterdir() if item.is_file()]
+        if files:
+            fname = files[0]
+    df = pd.read_csv(fname, comment="#")
+    return df
+
+
+def get_exoarchive(table="exomultpars"):
+    query = f"https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table={table}"
+    df = pd.read_csv(query, comment="#")
+
