bbe8ce3ef209c8b8c3edde54ba31af94e2f30f9a
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2020-05-15 20:05:34 +0200

    improve usage of query argument

--- sherlock/core.py
+++ sherlock/core.py
@@ -7,7 +7,7 @@ def get_min_value(col_name, df):
     cols = ["pl_name"] + [tmp.format(col_name) for tmp in ("{}", "{}err1", "{}err2")] + [ref_colname]
     df_aux = df[cols].copy()
     err_col = "{}_err".format(col_name)
-    df_aux = df_aux.assign(**{err_col: np.sqrt(df_aux[cols[2]]**2 + df_aux[cols[3]]**2)})
+    df_aux = df_aux.assign(**{err_col: (df_aux[cols[2]] + df_aux[cols[3]]) / 2})
     df_aux = df_aux.rename(columns={ref_colname: "{}_ref".format(col_name)})
     idxs = df_aux.groupby("pl_name")[err_col].idxmin().dropna()
     df_aux = df_aux.loc[idxs].drop(err_col, axis=1)
@@ -44,6 +44,9 @@ def update_columns_with_min_err(col_names, df_orig, df_fin, queries=None):
     for col_name in col_names:
         df_aux = df_orig
         if col_name in queries:
+            df_fin[col_name] = np.nan
             df_aux = df_orig.query(queries[col_name])
         df_fin = _update_column_with_min_err(col_name, df_aux, df_fin)
+    if queries:
+        return df_fin.dropna(subset=list(queries.keys()), how="any")
     return df_fin
