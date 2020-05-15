ebd503577507c9ecd221a43c99304061e5f7703c
Oriol (Prodesk) <oriol.abril.pla@gmail.com>
2020-05-15 21:50:12 +0200

    add function get_from_exoarchive

--- sherlock/core.py
+++ sherlock/core.py
@@ -1,10 +1,20 @@
+"""Core selection functions."""
+from typing import Iterable, Optional, Union, Mapping
 import numpy as np
+from pandas import DataFrame
 
-__all__ = ["get_min_value", "update_columns_with_min_err"]
+from .database import get_exoarchive
 
-def get_min_value(col_name, df):
+__all__ = ["update_columns_with_min_err", "get_from_exoarchive"]
+
+
+def _get_min_value(col_name, df):
     ref_colname = "{}_refname".format(col_name.split("_")[0])
-    cols = ["pl_name"] + [tmp.format(col_name) for tmp in ("{}", "{}err1", "{}err2")] + [ref_colname]
+    cols = (
+        ["pl_name"]
+        + [tmp.format(col_name) for tmp in ("{}", "{}err1", "{}err2")]
+        + [ref_colname]
+    )
     df_aux = df[cols].copy()
     err_col = "{}_err".format(col_name)
     df_aux = df_aux.assign(**{err_col: (df_aux[cols[2]] + df_aux[cols[3]]) / 2})
@@ -13,15 +23,23 @@ def get_min_value(col_name, df):
     df_aux = df_aux.loc[idxs].drop(err_col, axis=1)
     return df_aux
 
+
 def _update_column_with_min_err(col_name, df_orig, df_fin):
-    df_aux = get_min_value(col_name, df_orig).set_index("pl_name")
+    df_aux = _get_min_value(col_name, df_orig).set_index("pl_name")
     *cols, ref_col = df_aux.columns
     df_fin.loc[df_aux.index, cols] = df_aux[cols]
     df_fin[ref_col] = ""
     df_fin.loc[df_aux.index, ref_col] = df_aux[ref_col]
     return df_fin
 
-def update_columns_with_min_err(col_names, df_orig, df_fin, queries=None):
+
+def update_columns_with_min_err(
+    col_names: Union[str, Iterable[str]],
+    df_orig: DataFrame,
+    df_fin: DataFrame,
+    pre_queries: Optional[Mapping[str, str]] = None,
+    post_query: Optional[str] = None,
+) -> DataFrame:
     """Update columns of dataframe to a unique value per planet.
 
     For every column in the ``col_names`` the selected value will be the one with
@@ -39,14 +57,42 @@ def update_columns_with_min_err(col_names, df_orig, df_fin, queries=None):
     pandas.Dataframe
 
     """
-    if queries is None:
-        queries = {}
+    if pre_queries is None:
+        pre_queries = {}
     for col_name in col_names:
         df_aux = df_orig
-        if col_name in queries:
+        if col_name in pre_queries:
             df_fin[col_name] = np.nan
-            df_aux = df_orig.query(queries[col_name])
+            df_aux = df_orig.query(pre_queries[col_name])
         df_fin = _update_column_with_min_err(col_name, df_aux, df_fin)
-    if queries:
-        return df_fin.dropna(subset=list(queries.keys()), how="any")
+    if pre_queries:
+        df_fin = df_fin.dropna(subset=list(pre_queries.keys()), how="any")
+    if post_query:
+        return df_fin.query(post_query)
     return df_fin
+
+
+def get_from_exoarchive(
+    col_names: Union[str, Iterable[str]],
+    pre_queries: Optional[Mapping[str, str]] = None,
+    post_query: Optional[str] = None,
+) -> DataFrame:
+    """Download dataframe NASA Exoplanet Archive and query it.
+
+    Parameters
+    ----------
+    col_names : str of list of str
+    pre_queries : dict of {str: str}, optional
+    post_query : str, optional
+    """
+    exoarchive = get_exoarchive()
+    df_final = (
+        exoarchive.query("default_flag == 1")
+        .drop("default_flag", axis=1)
+        .set_index("pl_name")
+        .copy(deep=True)
+    )
+    df_final = update_columns_with_min_err(
+        col_names, exoarchive, df_final, pre_queries, post_query
+    )
+    return df_final
--- sherlock/database.py
+++ sherlock/database.py
@@ -1,19 +1,59 @@
+"""I/O capabilites."""
+from typing import Optional
 from pathlib import Path
 import pandas as pd
+from pandas._typing import FilePathOrBuffer
 
 __all__ = ["get_exoarchive", "get_local_exoarchive"]
 
-def get_local_exoarchive(fname=None):
+
+def get_local_exoarchive(
+    fname: Optional[FilePathOrBuffer] = None, default_pars: bool = False
+) -> pd.DataFrame:
+    """Load a local csv file downloaded from the NASA Exoplanet Archive.
+
+    Parameters
+    ----------
+    fname : str, optional
+        Name of the file to import as dataframe. By default, the csv file provided
+        by the package is loaded.
+    default_pars : bool, optional
+        If True, returns only the subset with ``default_flag == 1``.
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
     if fname is None:
         dirname = Path(__file__).parent / "data"
         files = [item for item in dirname.iterdir() if item.is_file()]
         if files:
             fname = files[0]
     df = pd.read_csv(fname, comment="#")
+    if default_pars:
+        return df.query("default_flag == 1")
     return df
 
 
-def get_exoarchive(table="exomultpars"):
+def get_exoarchive(
+    table: str = "exoplanets", extra_query: Optional[str] = None
+) -> pd.DataFrame:
+    """Download a table from the NASA Exoplanet Archive as a pandas.DataFrame.
+
+    Parameters
+    ----------
+    table : str, optional
+        Table to read from the NASA Exoplanet Archive.
+    extra_query : str, optional
+        String specifying a query in the format specified by the NASA Exoplanet
+        Archive API documentation. Should not start with ``&``.
+
+    Returns
+    -------
+    pandas.DataFrame
+    """
     query = f"https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table={table}"
+    if extra_query:
+        query = "&".join((query, extra_query))
     df = pd.read_csv(query, comment="#")
     return df
