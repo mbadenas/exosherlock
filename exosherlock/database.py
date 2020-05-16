18a8e2d0b0900336808edeb9bbc3926c5125002a
Oriol (ZBook) <oriol.abril.pla@gmail.com>
2020-05-16 17:58:08 +0200

    use explicit dtypes, merge local and remote loading
    fixes warning when reading the table

--- exosherlock/database.py
+++ exosherlock/database.py
@@ -4,71 +4,54 @@ from pathlib import Path
 import pandas as pd
 from pandas._typing import FilePathOrBuffer
 
-__all__ = ["get_exoarchive", "get_local_exoarchive"]
+from ._dtypes import DTYPES_CONFIRMED, DTYPES_PS
+
+__all__ = ["get_exoarchive"]
 
 
-def get_local_exoarchive(
-    fname: Optional[FilePathOrBuffer] = None, default_pars: bool = False
+def get_exoarchive(
+    table="exoplanets",
+    fname: Optional[FilePathOrBuffer] = None,
+    default_pars: bool = False,
+    local: bool=False,
+    **kwargs,
 ) -> pd.DataFrame:
-    """Load a local csv file downloaded from the NASA Exoplanet Archive.
-
-    Parameters
-    ----------
-    fname : str, optional
-        Name of the file to import as dataframe. By default, the csv file provided
-        by the package is loaded.
-    default_pars : bool, optional
-        If True, returns only the subset with ``default_flag == 1``.
-
-    Returns
-    -------
-    pandas.DataFrame
-    """
-    if fname is None:
-        dirname = Path(__file__).parent / "data"
-        files = [item for item in dirname.iterdir() if item.is_file()]
-        if files:
-            fname = files[0]
-    df = pd.read_csv(fname, comment="#")
-    if default_pars:
-        return df.query("default_flag == 1")
-    return df
-
-
-def get_remote_exoarchive(
-    table: str = "exoplanets", extra_query: Optional[str] = None
-) -> pd.DataFrame:
-    """Download a table from the NASA Exoplanet Archive as a pandas.DataFrame.
+    """Read a table form the NASA Exoplanet Archive as a pandas DataFrame.
 
     Parameters
     ----------
     table : str, optional
-        Table to read from the NASA Exoplanet Archive.
-    extra_query : str, optional
-        String specifying a query in the format specified by the NASA Exoplanet
-        Archive API documentation. Should not start with ``&``.
+        Which table from the exoplanet archive will be read. Used for custom dtypes
+        which reduce memory usage and allow extra functionalities compared to
+        automatic dtype parsing.
+    fname : str, optional
+        Name of the file to import as dataframe. By default, the csv file provided
+        by the package is loaded. Ignored if ``local==False``.
+    default_pars : bool, optional
+        If True, returns only the subset with ``default_flag == 1``.
+    local : bool, optional
+        When true, the local csv file at ``fname`` is read.
+    kwargs
+        Passed to :func:`pandas:pandas.read_csv`
 
     Returns
     -------
     pandas.DataFrame
     """
-    query = f"https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table={table}"
-    if extra_query:
-        query = "&".join((query, extra_query))
-    df = pd.read_csv(query, comment="#")
-    return df
-
-
-def get_exoarchive(*args, local: bool = False, **kwargs) -> pd.DataFrame:
-    """Load data from the Exoplanet Archive database.
-
-    Parameters
-    ----------
-    local : bool, optional
-    kwargs
-        Passed to :func:`get_remote_exoarchive` or :func:`get_local_exoarchive` depending
-        on the value of ``local``
-    """
     if local:
-        return get_local_exoarchive(*args, **kwargs)
-    return get_remote_exoarchive(*args, **kwargs)
+        if fname is None:
+            dirname = Path(__file__).parent / "data"
+            files = [item for item in dirname.iterdir() if item.is_file()]
+            if files:
+                fname = files[0]
+    else:
+        fname = f"https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table={table}"
+    kwargs.setdefault("comment", "#")
+    if table.lower() == "ps":
+        kwargs.setdefault("dtype", DTYPES_PS)
+    elif table.lower() in ("confirmed", "exoplanets"):
+        kwargs.setdefault("dtype", DTYPES_CONFIRMED)
+    df = pd.read_csv(fname, **kwargs)
+    if default_pars:
+        return df.query("default_flag == 1")
+    return df
